af067c0ddbb7d8cd205d360b2dd98b2d5630f7a2
angelamhchan <angelaming.chan@gmail.com>
2019-04-04 15:57:48 -0700

    Add review header and images of red and gray stars

--- client/src/components/ReviewEntry.jsx
+++ client/src/components/ReviewEntry.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/style.css';
+import redstar from '../redstar.png';
+import graystar from '../graystar.png';
+import UserEntry from './UserEntry.jsx'
 
 const ReviewEntry = ({ userId, overallScore, foodScore, serviceScore, ambienceScore, review }) => {
   return (
-    <div className="review-container">
-      <div className="cell userCell">{userId}</div>
-      <span className="cell scoreCell">
-        Overall {overallScore} Food {foodScore} Service {serviceScore} Ambience {ambienceScore}
-      </span>
-      <div className="cell reviewCell">{review}</div>
+    <div className="reviews-container">
+      <div className="reviews-user-cell">{userId}</div>
+      <div>
+        <span className="reviews-review-stars"><img src={redstar} alt="red star" height="16" width="auto" /></span>
+        <span className="reviews-review-stars"><img src={redstar} alt="red star" height="16" width="auto" /></span>
+        <span className="reviews-review-stars"><img src={redstar} alt="red star" height="16" width="auto" /></span>
+        <span className="reviews-review-stars"><img src={redstar} alt="red star" height="16" width="auto" /></span>
+        <span className="reviews-review-stars"><img src={graystar} alt="gray star" height="16" width="auto" /></span>
+        <span className="reviews-review-date">January 10, 2019</span>
+      </div>
+      <div className="reviews-score-cell">
+        <span className="reviews-score-category">Overall</span>
+        <span className="reviews-score-num">{overallScore}</span>
+        <span className="reviews-score-category">Food</span>
+        <span className="reviews-score-num">{foodScore}</span>
+        <span className="reviews-score-category">Service</span>
+        <span className="reviews-score-num">{serviceScore}</span>
+        <span className="reviews-score-category">Ambience</span>
+        <span className="reviews-score-num">{ambienceScore}</span>
+      </div>
+      <p className="reviews-review-cell">{review}</p>
     </div>
   );
 };
