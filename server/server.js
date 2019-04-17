0b9947f313de0287fe622e2908dbfd9654ce5409
Sutton <suco3693@gmail.com>
2019-04-17 10:47:25 -0700

    Update to use restaurant_id inplace of id for name of incoming value to match better with database

--- server/server.js
+++ server/server.js
@@ -14,16 +14,16 @@ db.sql.authenticate()
 const port = 3001;
 const app = express();
 
-app.use('/restaurants/:id', express.static(path.join(__dirname, '/../client/dist')));
+app.use('/restaurants/:restaurant_id', express.static(path.join(__dirname, '/../client/dist')));
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(morgan('tiny'));
 
-app.get('/api/restaurants/:id/reviews', (req, res) => {
-  const { id } = req.params;
-  Models.Review.findAll({ where: { restaurant_id: id }, include: [Models.User] })
+app.get('/api/restaurants/:restaurant_id/reviews', (req, res) => {
+  const { restaurant_id } = req.params;
+  Models.Review.findAll({ where: { restaurant_id: restaurant_id }, include: [Models.User] })
     .then((data) => {
       res.send(data);
       res.sendStatus(200);
@@ -36,3 +36,26 @@ app.listen(port, () => {
 });
 
 module.exports = app;
+
+
+/*
+
+  CRUD API ROUTES
+
+  Create
+  -Create new restauturant
+    -app.post(/api/restaurants/?name=name)
+  -create new user
+    -app.post(/api/user/?user=name)
+  -create new review
+    -app.post(/api/restaurants/:id/reviews/?review=review)
+
+   Read
+   -all reviews from 1 restaurant
+    -app.get('/api/restaurants/:id/reviews)
+  -all reviews from 1 user
+    -app.get('/api/user/:id)
+  -one review 
+    -app.get(/api/restaurants/:restaurant_id/reviews/::id)
+
+*/
