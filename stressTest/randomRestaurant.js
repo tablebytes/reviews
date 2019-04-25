39958ef59b45fe60991d58d605d840600251da1d
Sutton <suco3693@gmail.com>
2019-04-25 13:28:04 -0700

    Addition of stress test suite to test get path associated with restaurant id

--- stressTest/randomRestaurant.js
+++ stressTest/randomRestaurant.js
@@ -1,8 +1,10 @@
+'use strict';
 
-function randomRestaurant(context, events, done){
-  context.vars['random']= Math.floor(Math.random()*10000000);
+function randomRestaurant(userContext, events, done){
+  var random = Math.floor(Math.random()*10000000);
+  userContext.vars.random = random;
   return done();
 }
 
 
-module.exports = randomRestaurant;
+module.exports = {randomRestaurant};
